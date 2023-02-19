76066b4416200cf125cddbe8bb9df9b16ba14d44
LAPTOP-THHEP6J5\Uros <urosrajkovic011@hotmail.com>
2023-02-19 17:31:21 +0100

    TaliwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+let tailwindcss = require('tailwindcss');
+module.exports = {
+  content: [
+    "./assets/**/*.{vue,js,ts,jsx,tsx}",
+    "./templates/**/*.{html,twig}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    
+  ]
+}
