b5f843f4bf85f3c691139cfc8604dce18f652341
LAPTOP-THHEP6J5\Uros <urosrajkovic011@hotmail.com>
2023-02-19 13:40:37 +0100

    entity repo

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,16 +21,30 @@ class MoviesController extends AbstractController
     }
     */
     #[Route('/movies', name: 'app_movies')]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-         $movies =['uros', 'sara','sonja', 'ljuba','deja','milos'];
+      //drugi metod (EntityManagerRepository $em)
+      //sa konstruktorm na vrhu controlora(asp.net ist prinicp)
+      
+      //$movies =['uros', 'sara','sonja', 'ljuba','deja','milos'];
          /*
        return $this->render('index.html.twig',[
         'title'=> 'avangers: end game'
        ]);
-       */
+       
       return $this->render('index.html.twig',array(
         'movies' => $movies
       ));
+      */
+
+      //find()
+      //findAll()
+      //findBy()
+
+
+       $movies = $movieRepository->findAll();
+        dd($movies);
+      return $this->render('index.html.twig');
+
     }
 }
